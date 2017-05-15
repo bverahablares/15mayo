275350834233c708a9e0ba016d46117b07270d3a
bverahablares <bverahablares@gmail.com>
2017-05-15 13:18:52 -0400

    Update about.php

--- about.php
+++ about.php
@@ -7,7 +7,9 @@ array_walk($datos, function(&$a) use ($datos) {$a = array_combine($datos[0], $a)
 array_shift($datos);
 ?>
 <h2>Sobre el proyecto</h2>
-<p>Acá peguen sus about</p>
+<p>Dentro de las artes escénicas la tecnología está aportando cada vez más en cuanto a imagen sobre el escenario y composición escénica visual en general. Es en este  punto donde la proyección de imagen comienza a formar parte de las nuevas formas de exponer mundos nuevos o complementación de imagen y sonido, en el campo de los show musicales.</p>
+<p>Este trabajo busca conocer cómo las proyecciones en los últimos tiempos, donde la tecnología se ha incorporado cada vez más, intenta aportar en las artes escénicas llevando a la audiencia a otro nivel de interpretación al momento de ver un show, con el fin de crear un repertorio y secuencias de imágenes animadas que permitan incorporar la imagen a los show musicales, donde no sólo la música es primordial sino que el ambiente que se produzca genere y comunique algo más allá.</p>
 
 </section>
 <?php include('pie.php');?>
+
