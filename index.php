6e80a326e35ab1725a4718b9e839f06fb8d77c64
bverahablares <bverahablares@gmail.com>
2017-05-15 13:19:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <h2>Artículos</h2>
 <?php
 // basta con la línea de PHP para llamar al imdb-movies.csv y asignarlo a la variable $csv
-$csv = array_map('str_getcsv', file('data/datos.csv'));
+$csv = array_map('str_getcsv', file('datos.csv'));
 // pero debo hacer un pequeño ajuste, para eliminar del arreglo el encabezado del imdb-movies.csv
 array_walk($csv, function(&$a) use ($csv) {$a = array_combine($csv[0], $a);});
 array_shift($csv);
@@ -12,10 +12,11 @@ array_shift($csv);
 for($a = 0; $a < $total = count($csv); $a++){?>
   <article class="row">
     <hr>
-    <div class="col-xs-5 col-sm-3 col-md-2"><img src="<?php echo $csv[$a]['Imagen'];?>" class="img-responsive"></div>
-    <div class="col-xs-7 col-sm-9 col-md-10">
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
       <h3><?php echo $csv[$a]['id'];?>. <?php echo $csv[$a]['titulo'];?> </h3>
       <h5>Referencia <?php echo $csv[$a]['apa'];?> </h5>
+      <img src="<?php echo $csv[$a]['images'];?>" class="img-responsive">
       <p><strong>Abstract:</strong> <?php echo $csv[$a]['texto'];?></p>
       <p><strong>Vínculo:</strong></p><?php echo $csv[$a]['vinculo'];?></p>
       <p><strong>Palabras Clave:</strong></p><?php echo $csv[$a]['tags'];?></p>
